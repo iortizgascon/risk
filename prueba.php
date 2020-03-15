6c8d726bd48ab326de166a147ca8b8deebfb5f6d
Iñaki <iortizgascon@gmail.com>
2020-03-15 19:51:57 +0100

    Reiniciando

--- prueba.php
+++ prueba.php
@@ -1,7 +1,8 @@
 <?php 
 
-/*include($_SERVER["DOCUMENT_ROOT"] . "init.php");
+include($_SERVER["DOCUMENT_ROOT"] . "init.php");
 
+/*
 $sql = "SELECT * FROM partida";
 $datos = $db->fetchAll($sql);
 
